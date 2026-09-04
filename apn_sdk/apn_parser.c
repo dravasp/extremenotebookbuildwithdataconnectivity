7558049a4d6e3aea4aa1ff4a0031e8ac68ce5a8f
DRAVASP SHROFF <27689043+dravasp@users.noreply.github.com>
2026-09-04 21:52:11 +0530

    Add functions to parse APN XML and mobileconfig

---
+++ apn_sdk/apn_parser.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+int parse_apn_xml(const char* filename) { printf("Parsing XML: %s\n", filename); return 0; }
+int parse_apn_mobileconfig(const char* filename) { printf("Parsing mobileconfig: %s\n", filename); return 0; }
